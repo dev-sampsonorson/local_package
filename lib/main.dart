d30184956332df70eac5b2694793ad069aed3f35
Sampson Orson Jackson <sampson.orson@gmail.com>
2023-05-23 19:08:02 +0100

    chore: workspace setup and startup code

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:local_package/pages/home_page.dart';
+
+void Main() {
+  runApp(const RootApp());
+}
+
+class RootApp extends StatelessWidget {
+  const RootApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Local Package',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const HomePage(),
+    );
+  }
+}
